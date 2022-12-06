cdf75a0b5a2709c39762c141c54ed1ff5a831794
Franco Silva <franco.silva4477@alumnos.udg.mx>
2022-12-05 19:27:51 -0600

    Barra de progreso, activar y desactivar boton, radio buttons, telefono

--- js/preguntas.js
+++ js/preguntas.js
@@ -1,5 +1,11 @@
 let preguntas = ["2.- Suelo caer en tentaciones que me dificultan cumplir con un compromiso", "3.- Busco conseguir beneficios inmediatos, en vez de esperar algo mejor más tarde", "4.- Continúo haciendo determinadas actividades placenteras a pesar de que los demás me advierten que me perjudican", "5.- Cuando algo se me antoja voy a por ello de forma inmediata, sin poder esperar"]
 var i=0
+let intervalo = 100/preguntas.length;
+let progreso = 0
+
+window.addEventListener("beforeunload", function(event) {
+    event.returnValue = "Write something clever here..";
+  });
 async function onload(){
     bt= document.getElementById("siguiente");
     bt.addEventListener("click", ()=>{
@@ -7,6 +13,7 @@ async function onload(){
         i=i+1
      })
 }
+
 
 async function activar(element){
     bt= document.getElementById("siguiente");
@@ -26,4 +33,16 @@ async function pasar(){
         btn.id="resultados";
         document.getElementById("resultados").setAttribute("onclick", "enviar()")
     }
+    console.log(preguntas.length)
+    progreso = intervalo + progreso
+    console.log(progreso)
+    document.getElementById("barra").setAttribute("style", "width: "+progreso+"%")
+    document.getElementById("barra").setAttribute("aria-valuenow", progreso)
+    document.getElementById("btnradio1").checked = false;
+    document.getElementById("btnradio2").checked = false;
+    document.getElementById("btnradio3").checked = false;
+    document.getElementById("btnradio4").checked = false;
+    document.getElementById("btnradio5").checked = false;
+    bt= document.getElementById("siguiente");
+    bt.disabled = true;
 }
